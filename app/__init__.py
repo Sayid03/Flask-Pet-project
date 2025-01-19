bef2c1f5c657aaded0fe7f7a83db46a00432f414
kermit0103 <egamberdiefsayid@gmail.com>
2025-01-19 15:45:02 +0500

    replacing sqlite with postgresql

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from .extentions import db
+from .extensions import db
+from .config import Config
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+    
+    app.config.from_object(config_class)
 
     db.init_app(app)
+
     with app.app_context():
         db.create_all()
 
---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os 
+
+class Config(object):
+    USER = os.environ.get('POSTGRES_USER', 'kermit0103')
+    PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'qwertywsad12')
+    HOST = os.environ.get('POSTGRES_HOST', '127.0.0.1')
+    PORT = os.environ.get('POSTGRES_PORT', 5532)
+    DB = os.environ.get('POSTGRES_DB', 'mydb')
+
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+    SECRET_KEY = 'IjustWannaseeUrb00bs100Timesasdhheqada823'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
