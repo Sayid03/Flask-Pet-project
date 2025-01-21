0bbfab1f18eff42b909f7e2e9b5cc8152e23fde7
kermit0103 <egamberdiefsayid@gmail.com>
2025-01-21 13:08:57 +0500

    new adding post function

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,12 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
 from ..models.post import Post
 from ..extensions import db
 
 post = Blueprint('post', __name__)
 
-@post.route('/post/<subject>')
-def create_post(subject):
-    post = Post(subject=subject)
-    db.session.add(post)
-    db.session.commit()
-    return f'Subject - {subject} Created Successfully!'
+@post.route('/post/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        teacher = request.form.get('teacher')
+        subject = request.form.get('subject')
+        student = request.form.get('student')
+
+        post = Post(teacher=teacher, subject=subject, student=student)
+        
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(str(e))
+
+    else:
+        return render_template('post/create.html')
