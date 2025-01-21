611965c391d6537b096c20938bddca28684ead5b
kermit0103 <egamberdiefsayid@gmail.com>
2025-01-21 12:17:41 +0500

    working with html/css & migrating db

--- app/models/post.py
+++ app/models/post.py
@@ -1,5 +1,9 @@
+from datetime import datetime
 from ..extensions import db
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    teacher = db.Column(db.String(258))
     subject = db.Column(db.String(258))
+    student = db.Column(db.String(258))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
