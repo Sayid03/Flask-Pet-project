368511bf6ea342a6052bbbddf58368694ab1223b
kermit0103 <egamberdiefsayid@gmail.com>
2025-01-23 20:22:46 +0500

    sign up validation

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, FileField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+
+from .models.user import User
+from .extensions import db
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
@@ -10,3 +13,8 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     avatar = FileField('Upload your Photo', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
     submit = SubmitField('Sign Up!')
+
+    def validate_login(self, login):
+        user = User.query.filter_by(login=login.data).first()
+        if user:
+            raise ValidationError(f'Login - {login.data} is already taken! Please take another one')
